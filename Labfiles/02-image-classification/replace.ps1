8fcdffec317009d089ddec5ddcbaccf97e7e98d7
Lars Warholm <lars.warholm@bouvet.no>
2024-08-26 14:47:33 +0200

    Ex 2: yet more preliminaries

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -1,3 +1,5 @@
-param([string]$storageAcct) 
+param([string]$storageAcct)
+
+Copy-Item .\training-images\training_labels_orig.json .\training-images\training_labels.json
 
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
